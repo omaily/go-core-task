c19437a9a986f03bdf52c550010d8a9b493659ca
oldguard <123zedc@mail.ru>
2024-12-06 17:33:43 +1000

    complete task 1.5

--- task_1/main1.go
+++ task_1/main1.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"reflect"
 	"strings"
@@ -28,6 +31,17 @@ func convertString(arr []interface{}) string {
 	return buffer.String()
 }
 
+func saltLine(str []rune) []byte {
+	var buffer bytes.Buffer
+	for i, v := range str {
+		if i == len(str)/2 {
+			buffer.WriteString("go-2024")
+		}
+		buffer.WriteRune(v)
+	}
+	return buffer.Bytes()
+}
+
 func main() {
 	// 1.1
 	arr := make([]interface{}, 0)
@@ -36,21 +50,36 @@ func main() {
 	arr = append(arr, _hex(0x2A))
 	arr = append(arr, 3.14)
 	arr = append(arr, 1+2i)
-	arr = append(arr, "Golang")
+	arr = append(arr, "Golangs")
 	arr = append(arr, true)
 
 	// 1.2
+	fmt.Print("par.2: ")
 	for _, v := range arr {
-		fmt.Println(checkType(v))
+		fmt.Print(checkType(v), " ")
 	}
+	fmt.Println()
 
 	//1.3
+	fmt.Print("par.3: ")
 	str := convertString(arr)
-	fmt.Println(convertString(arr))
+	fmt.Print(str)
+	fmt.Println()
 
 	//1.4
+	fmt.Print("par.4: ")
 	textUTF := []rune(str)
 	for _, v := range textUTF {
 		fmt.Printf("%c", v)
 	}
+	fmt.Println()
+
+	//1.5
+	fmt.Print("par.5: ")
+	textByte := saltLine(textUTF)
+
+	hasher := sha256.New()
+	hasher.Write(textByte)
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	fmt.Println(sha)
 }
