b0d4e65c8a445acd9f7380da59fe5480d7d7dbd6
oldguard <123zedc@mail.ru>
2024-12-07 15:59:36 +1000

    complete task 2.2

--- task_2/task2.go
+++ task_2/task2.go
@@ -6,15 +6,31 @@ import (
 )
 
 func createSlise(n int) []int {
-	slise := make([]int, n)
-	for i := range slise {
-		slise[i] = rand.Intn(10)
+	sliсe := make([]int, n)
+	for i := range sliсe {
+		sliсe[i] = rand.Intn(10)
 	}
-	return slise
+	return sliсe
+}
+
+func sliceExample(sliсe []int) []int {
+	even := make([]int, 0, len(sliсe))
+	for _, v := range sliсe {
+		if v%2 == 0 {
+			even = append(even, v)
+		}
+	}
+
+	return even
 }
 
 func StartTask2() {
 	// 2.1
 	originalSlice := createSlise(10)
 	fmt.Println("task 2:", originalSlice)
+
+	// 2.2
+	evenSlice := sliceExample(originalSlice)
+	fmt.Println("task 2:", evenSlice)
+
 }
