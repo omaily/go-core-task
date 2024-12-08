18001a5755115a2b7fca2ac093acd9e51db36871
oldguard <123zedc@mail.ru>
2024-12-08 22:28:02 +1000

    start with WaitGroup

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import task "coretask/task_8"
 
 func main() {
-	task.StartTask8()
+	task.StartTask8(1000)
 }
--- task_8/task8.go
+++ task_8/task8.go
@@ -1,7 +1,26 @@
 package task_8
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func StartTask8() {
+var counter = 0
+var wg sync.WaitGroup
+
+func incrementCounter() {
+	counter++
+	wg.Done()
+}
+
+func StartTask8(count int) {
 	fmt.Println("Start task 8")
+
+	for i := 0; i < count; i++ {
+		wg.Add(1)
+		go incrementCounter()
+	}
+
+	wg.Wait()
+	fmt.Println("data-in:", count, ", result increment:", counter)
 }
