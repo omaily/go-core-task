4d45470d1fa47c882c331fa447ce5a3d11e6a5d5
oldguard <123zedc@mail.ru>
2024-12-13 11:26:45 +1000

    complete task7

--- task_7/task7.go
+++ task_7/task7.go
@@ -1,7 +1,55 @@
 package task_7
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func joinChannels(chs ...<-chan int) <-chan int {
+	mergedCh := make(chan int)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(chs))
+	go func() {
+		for _, ch := range chs {
+			go func() {
+				for id := range ch {
+					mergedCh <- id
+				}
+				wg.Done()
+			}()
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(mergedCh)
+	}()
+
+	return mergedCh
+}
 
 func StartTask7() {
-	fmt.Println("Start task 7")
+
+	a := make(chan int)
+	b := make(chan int)
+
+	go func() {
+		for v := range 5 {
+			a <- v + 1
+		}
+		close(a)
+	}()
+
+	go func() {
+		for v := range 5 {
+			a <- (v + 1) * 10
+		}
+		close(b)
+	}()
+
+	for num := range joinChannels(a, b) {
+		fmt.Println(num)
+	}
+
 }
